a3d2545f90f3d89e1144d60e3c3f5a37ab8e31e5
Nikolas9896 <nhlynka@gmail.com>
2019-09-10 15:32:51 +0300

    change route /fallinginlovewith/:thing

--- MoreExpress/EJS_Demo/app.js
+++ MoreExpress/EJS_Demo/app.js
@@ -3,7 +3,12 @@ var express = require("express");
 var app = express();
 
 app.get("/", function(req, res){
-    res.send("Welcome to the home page!");
+    res.render("home.ejs");
+});
+
+app.get("/fallinginlovewith/:thing", function(req, res){
+    var thing = req.params.thing;
+    res.render("love.ejs");
 });
 
 app.listen(3000, function(){
