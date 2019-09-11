c96c393e37a413d3f720e17b4315c11f1aa4aedd
Nikolas9896 <nhlynka@gmail.com>
2019-09-11 18:37:19 +0300

    repair request to google.com

--- MoreExpress/APIs/first_request.js
+++ MoreExpress/APIs/first_request.js
@@ -1,7 +1,9 @@
 var request = require("request");
 
-request('http:/www.google.com', function(error, response, body){
+request('http://www.google.com', function(error, response, body){
     if(!error && response.statusCode == 200){
         console.log(body)//show the HTML for the Google homepage.
+    } else {
+        console.log(error);
     }
 });
