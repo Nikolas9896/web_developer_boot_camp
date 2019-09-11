09e460fd379f25cafd71f271c23aeaa8b4bf8f1d
Nikolas9896 <nhlynka@gmail.com>
2019-09-11 13:48:27 +0300

    add friends page and display the friends list

--- MoreExpress/PostRequestDemo/app.js
+++ MoreExpress/PostRequestDemo/app.js
@@ -8,6 +8,12 @@ app.get("/", function(req, res){
     res.render("home");
 });
 
+app.get("/friends", function(req, res){
+    var friends = ["Volodymyr", "Yuriy", "Bogdan", "Yaroslav", "Roman", "Nazar"];
+    res.render("friends", {friends: friends});
+});
+
+
 app.listen(3000, function(){
     console.log("Server started! PORT 3000!");
 });
