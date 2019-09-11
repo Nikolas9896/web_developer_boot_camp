060a051d25515c40e11a97e6b626b3f520fdb228
Nikolas9896 <nhlynka@gmail.com>
2019-09-11 10:54:32 +0300

    use express.static for public directory with app.css - style file and add setup for ejs files now it's no need to write filename.ejs write only filename

--- MoreExpress/EJS_Demo/app.js
+++ MoreExpress/EJS_Demo/app.js
@@ -2,13 +2,15 @@ var express = require("express");
 
 var app = express();
 
+app.use(express.static("public"));
+app.set("view engine", "ejs");
 app.get("/", function(req, res){
     res.render("home.ejs");
 });
 
 app.get("/fallinginlovewith/:thing", function(req, res){
     var thing = req.params.thing;
-    res.render("love.ejs", {thingVar: thing});
+    res.render("love", {thingVar: thing});
 });
 
 app.get("/posts", function(req, res){
@@ -17,7 +19,7 @@ app.get("/posts", function(req, res){
         { title: "Post 2", author: "Anastasiia"},
         { title: "Post 3", author: "Anonymous"}
     ];
-    res.render("posts.ejs", {posts: posts_here});
+    res.render("posts", {posts: posts_here});
 });
 
 app.listen(3000, function(){
