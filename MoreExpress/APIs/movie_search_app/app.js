d590dccf6b9856a4c5df72259384ad5c3b5377e5
Nikolas9896 <nhlynka@gmail.com>
2019-09-12 13:47:39 +0300

    add prepare route for results ejs template

--- MoreExpress/APIs/movie_search_app/app.js
+++ MoreExpress/APIs/movie_search_app/app.js
@@ -2,12 +2,14 @@ var express = require("express");
 
 var app = express();
 var request = require("request");
+app.set("view engine", "ejs");
 
 app.get("/results", (req, res) => {
     request("http://www.omdbapi.com/?s=guardians+of+the+galaxy&apikey=thewdb", (error, response, body) => {
      if(!error && response.statusCode == 200){
-         var results = JSON.parse(body)
-         res.send(results["Search"][0]);
+         var data = JSON.parse(body)
+         res.render("results", {data: data});
+         //res.send(results["Search"][0]["Title"]);
      }   
     });
 });
