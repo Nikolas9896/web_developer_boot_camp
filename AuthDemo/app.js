ea9ed65d526c5ba7c7f8305e9ffda3b7f4e231ac
Nikolas9896 <nhlynka@gmail.com>
2019-09-25 12:54:02 +0300

    require all passport items

--- AuthDemo/app.js
+++ AuthDemo/app.js
@@ -1,5 +1,9 @@
-var express     = require("express"),
-mongoose        = require("mongoose");
+var express             = require("express"),
+mongoose                = require("mongoose"),
+passport                = require("passport"),
+LocalStrategy           = require("passport-local"),
+bodyParser              = require("body-parser"),
+passportLocalMongoose   = require("passport-local-mongoose");
 
 mongoose.connect("mongodb://localhost:27017/auth_demo_app", {useNewUrlParser: true });
 
